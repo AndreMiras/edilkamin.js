10b4553992ae6f524566f6b01678f32012f0a634
Andre Miras <andre.miras@gmail.com>
2022-02-22 22:41:46 +0100

    :sparkles: feat: Temperature info

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
-export { DeviceInfoType } from "./types";
+export {
+  CommandsType,
+  DeviceInfoType,
+  StatusType,
+  TemperaturesType,
+  UserParametersType,
+} from "./types";
 
 export { main, deviceInfo, setPower, setPowerOff, setPowerOn } from "./library";
--- src/types.ts
+++ src/types.ts
@@ -2,12 +2,35 @@ interface CommandsType {
   power: boolean;
 }
 
+interface TemperaturesType {
+  board: number;
+  enviroment: number;
+}
+
 interface StatusType {
   commands: CommandsType;
+  temperatures: TemperaturesType;
+}
+
+interface UserParametersType {
+  enviroment_1_temperature: number;
+  enviroment_2_temperature: number;
+  enviroment_3_temperature: number;
+  is_auto: boolean;
+  is_sound_active: boolean;
 }
 
 interface DeviceInfoType {
   status: StatusType;
+  nvm: {
+    user_parameters: UserParametersType;
+  };
 }
 
-export type { DeviceInfoType };
+export type {
+  CommandsType,
+  DeviceInfoType,
+  StatusType,
+  TemperaturesType,
+  UserParametersType,
+};
